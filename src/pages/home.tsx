82dabe0e13f93030c405d7da69acaa72f312c44c
DavidjFilho <david_curso@hotmail.com>
2026-03-19 20:37:04 -0300

    chore(setup): adicionado shadcn

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,14 @@
+import { CheckCircle2Icon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div className="grid w-full max-w-md items-start gap-4">
+      <Alert variant="warn">
+        <CheckCircle2Icon className="size-8" />
+        <AlertTitle>Bem-vindo a Home</AlertTitle>
+        <AlertDescription>Meu primeiro componente</AlertDescription>
+      </Alert>
+    </div>
+  );
 }
--- todo.md
+++ todo.md
@@ -5,7 +5,7 @@
 - [x] Iniciar projeto com [Vite](https://tailwindcss.com/docs/installation/using-vite)
 - [x] Implementar Tailwind no projeto
 - [x] Aplicar [React Routes](https://reactrouter.com/start/declarative/installation)
-- [ ] Implementar [Shadcn](https://ui.shadcn.com/)
+- [x] Implementar [Shadcn](https://ui.shadcn.com/)
 - [ ] Implementar Eslint
 
 
